d9c4df8740614daffd682a9ebe5447f1bdad9111
Jing Xue <97304545+jingxue8303@users.noreply.github.com>
2022-06-30 14:31:11 +0000

    completed Nim

--- programming/1/Nim.java
+++ programming/1/Nim.java
@@ -1,11 +1,37 @@
 import java.io.*;
 import java.util.*;
-
+import java.util.Scanner;
+import java.util.Random;
 
 public class Nim{
   public static void main (String[] args){
+    int stone = 12;
+    int stoneTaken = 0;
+    int stoneRemain = stone - stoneTaken;
     
-  
-  System.out.println("Hello");
+    while (stoneRemain != 0){
+      Scanner input = new Scanner(System.in);
+      System.out.print("Choose the number of stones you want to pick, from 1 to 3: ");
+      int player = input.nextInt();
+      stoneTaken = player + stoneTaken ;
+      stoneRemain = stone - stoneTaken;
+      System.out.println("Player took " + player + " stones");
+      System.out.println("Stone remaining: " + stoneRemain);
+      if (stoneRemain ==0){
+        System.out.println("Player wins");
+        break;
+      }
+      Random random = new Random();
+      int machine = random.nextInt(3) + 1;
+      System.out.println("Machine took " + machine + " stones");
+      stoneTaken = stoneTaken + machine;
+      stoneRemain = stone - stoneTaken;
+      System.out.println("Stone remaining: " + stoneRemain);
+      if (stoneRemain ==0){
+        System.out.println("Machine wins");
+        break;
+      }
+    }
+    System.out.println("The game has ended.");
   }
 }
